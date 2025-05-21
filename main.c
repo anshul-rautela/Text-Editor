56b9f09d3636ab59cd3be0a48d0bfdcec620ec72
pulkityadav245 <pulkityadavedu245@gmail.com>
2025-05-21 12:04:37 +0530

    Update main.c
    added update window title at line number 27

--- main.c
+++ main.c
@@ -23,6 +23,9 @@ int main(int argc, char *argv[]) {
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Simple GTK Text Editor");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+    update_window_title(GTK_WINDOW(window), NULL); ///   PLACE for window title
+    
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
